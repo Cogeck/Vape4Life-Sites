d26aa140db2162cf6116706bb624a53da5a533bb
Cogeck <Dennis-schwarze@gmx.de>
2016-11-06 16:29:45 +0100

    More dynamic Reload feture
    More dynamic Reload feture

---
+++ js/Comunicate.js
@@ -0,0 +1,47 @@
+function ComunicateWithServer(jsonData, functionName, succesCallback, deniedCallback){	
+	$.ajax({	
+	 type: "POST",
+     url: "http://localhost:63913/Services/ServiceController.asmx/"+functionName+"",
+     crossDomain:true,
+	
+	 beforeSend: function(xhr) {
+       xhr.setRequestHeader("Content-type", 
+                            "application/json; charset=utf-8");
+     },
+	
+	 data: JSON.stringify(jsonData),
+	 dataType: 'json',
+	 cache: false,
+	
+     success: function(msg) {		
+		if(msg.d.AlloewdToReload === true){		
+
+			for(var i = 0; msg.d.JS.length > i; i++){
+				var js  = msg.d.JS[i];				
+				$.getScript( js, function( data, textStatus, jqxhr ) {
+				  console.log( data ); // Data returned
+				  console.log( textStatus ); // Success
+				  console.log( jqxhr.status ); // 200
+				  console.log( "Load was performed." );
+				});	
+			}
+			
+			for(var i = 0; msg.d.CSS.length > i; i++){
+				var cs  = msg.d.CSS[i];						
+				$("head").append('<link rel="stylesheet" href="'+cs+'" type="text/css">');
+			}
+		
+			for(var i = 0; msg.d.HTML.length > i; i++){
+				var html = msg.d.HTML[i];				
+				succesCallback(html);
+			}										
+		}
+		else{
+			deniedCallback();
+		}
+     },
+	 error: function(msg) {
+		alert("Ein Fehler ist aufgetreten, bitte informieren Sie unseren Support.");		
+	 }
+ });
+}
--- js/login.js
+++ js/login.js
@@ -1,42 +1,15 @@
-function Login(user, pw){
-	   
-var userData = {email:user,password:pw};
-var userObject = {user:JSON.stringify(userData)};
+function succesCallback(html){
+	$('#ReloadContainer').load(html);	
+}
 
- $.ajax({
-	
-	 type: "POST",
-     url: "http://localhost:63913/Services/ServiceController.asmx/CheckIfUserAlloewdAccess",
-     crossDomain:true,
-	
-	 beforeSend: function(xhr) {
-       xhr.setRequestHeader("Content-type", 
-                            "application/json; charset=utf-8");
-     },
-	
-	 data: JSON.stringify(userObject),
-	 dataType: 'json',
-	 cache: false,
-	
-     success: function(msg) {		
-		if(msg.d.HTML != "NOPE"){ 			
+function deniedCallback (){	
+	$('#ReloadContainer').empty();
+	$('#ReloadContainer').append("Bitte versuchen sie es noch einmal");
+}
 
-			$.getScript( msg.d.JS, function( data, textStatus, jqxhr ) {
-				  console.log( data ); // Data returned
-				  console.log( textStatus ); // Success
-				  console.log( jqxhr.status ); // 200
-				  console.log( "Load was performed." );
-			});			
-			
-			$('#ReloadContainer').load(msg.d.HTML);
-		}
-		else{
-			$('#ReloadContainer').empty();
-			$('#ReloadContainer').append("Bitte versuchen sie es noch einmal");
-		}
-     },
-	 error: function(msg) {
-		alert("ERROR");		
-	 }
- });
+function Login(user, pw){	   
+	var userData = {email:user,password:pw};
+	var userObject = {user:JSON.stringify(userData)};
+
+	ComunicateWithServer(userObject,"CheckIfUserAlloewdAccess", succesCallback, deniedCallback);
 }
